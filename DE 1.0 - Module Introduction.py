ca810ac3dbd96a0d14243325f70fd50d693b5112
vaclav.barina@ekimetrics.com <vaclav.barina@ekimetrics.com>
2024-07-29 18:51:15 +0000

    Testing a dummy change

--- DE 1.0 - Module Introduction.py
+++ DE 1.0 - Module Introduction.py
@@ -10,7 +10,7 @@
 
 # MAGIC %md
 # MAGIC ## Introduction to the Databricks Workspace for Data Engineers
-# MAGIC This module is part of the Data Engineer Learning Path by Databricks Academy. <br>
+# MAGIC This module is part of the Learning Path by Databricks Academy. <br>
 # MAGIC
 # MAGIC #### Lessons
 # MAGIC | Type | Lesson |
